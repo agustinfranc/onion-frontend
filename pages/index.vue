3f488910849bd5411c263e2181b5b1994ceae1f3
Agustin Amato <agustin.amato96@gmail.com>
2020-08-26 22:50:19 -0300

    [landing] agrego landing-page

--- pages/index.vue
+++ pages/index.vue
@@ -1,19 +1,490 @@
 <template>
-  <v-layout column justify-center align-center>
-    <v-flex xs12 sm8 md6>
-      <h2>Index</h2>
-    </v-flex>
-  </v-layout>
+  <div>
+    <!--[if lte IE 9]>
+              <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="https://browsehappy.com/">upgrade your browser</a> to improve your experience and security.</p>
+    <![endif]-->
+
+
+    <!-- header-start -->
+    <header>
+      <div class="header-area">
+        <div id="sticky-header" class="main-header-area" style="max-height: 75px">
+          <div class="container-fluid">
+            <div class="row align-items-center">
+              <div class="col-xl-3 col-lg-2 px-1 py-0">
+                <div class="logo">
+                  <a href="index.html">
+                    <img src="img/logo.png" alt />
+                  </a>
+                </div>
+              </div>
+              <div class="col-xl-6 col-lg-7 d-none d-lg-block">
+                <div class="main-menu d-none d-lg-block">
+                  <nav>
+                    <ul id="navigation">
+                      <li>
+                        <a href="index.html" class="active">home</a>
+                      </li>
+                      <li>
+                        <a href="#service_area_2">Caracteristicas</a>
+                      </li>
+                      <li>
+                        <a href="#prising_area">Planes</a>
+                      </li>
+                      <li>
+                        <a href="#contact_area">Contacto</a>
+                      </li>
+                      <li>
+                        <a href="#partners_area">Clientes</a>
+                      </li>
+                    </ul>
+                  </nav>
+                </div>
+              </div>
+              <div class="col-xl-3 col-lg-3 d-none d-lg-block">
+                <!-- <div class="Appointment">
+                                  <div class="book_btn d-none d-lg-block">
+                                      <a  href="#">Download</a>
+                                  </div>
+                </div>-->
+              </div>
+              <div class="col-12">
+                <div class="mobile_menu d-block d-lg-none"></div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </header>
+    <!-- header-end -->
+
+    <!-- slider_area_start -->
+    <div id="slider_area" class="slider_area">
+      <div class="single_slider d-flex align-items-center slider_bg_1">
+        <div class="container">
+          <div class="row align-items-center">
+            <div class="col-xl-7 col-md-6">
+              <div class="slider_text">
+                <h3 class="wow fadeInDown" data-wow-duration="1s" data-wow-delay=".1s">
+                  Vendé más con tu Tienda Online
+                  <br />
+                </h3>
+                <p
+                  class="wow fadeInLeft"
+                  data-wow-duration="1s"
+                  data-wow-delay=".1s"
+                >Desde tu carta digital hasta tu App a medida</p>
+                <div
+                  class="video_service_btn wow fadeInLeft"
+                  data-wow-duration="1s"
+                  data-wow-delay=".1s"
+                >
+                  <a href="#service_area_2" class="boxed-btn3">Comenzar</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-xl-5 col-md-6">
+              <div class="phone_thumb wow fadeInDown" data-wow-duration="1.1s" data-wow-delay=".2s">
+                <img src="img/ilstrator/phone.png" alt />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- slider_area_end -->
+
+    <!-- service_area_2  -->
+    <div id="service_area_2" class="service_area_2">
+      <div class="container">
+        <div class="row align-items-center">
+          <div class="col-xl-12">
+            <div
+              class="section_title text-center wow fadeInUp"
+              data-wow-duration=".5s"
+              data-wow-delay=".3s"
+            >
+              <h3>
+                Ahora tu menú
+                <br />de forma digital
+              </h3>
+            </div>
+          </div>
+
+          <div
+            class="col-xl-4 col-lg-4 col-md-6 wow fadeInLeft"
+            data-wow-duration=".4s"
+            data-wow-delay=".5s"
+          >
+            <div class="man_thumb">
+              <img src="img/ilstrator_img/qr_hand.png" alt />
+            </div>
+          </div>
+          <div class="col-xl-5 offset-xl-2 col-lg-6 col-md-6">
+            <div class="mobile_screen wow fadeInRight" data-wow-duration=".8s" data-wow-delay=".5s">
+              <img src="img/ilstrator_img/mobile_screen.png" alt />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--/ service_area_2  -->
+
+    <!-- service_content_area  -->
+    <div class="service_content_area">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-4 col-md-4">
+            <div class="single_service wow fadeInUp" data-wow-duration=".5s" data-wow-delay=".3s">
+              <h3>Simple</h3>
+              <p>QR intuitivo con la carta digital para utilizar en las mesas del salón.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4">
+            <div class="single_service wow fadeInUp" data-wow-duration=".6s" data-wow-delay=".4s">
+              <h3>Rápido</h3>
+              <p>Desde cualquier celular, el cliente elige de forma directa.</p>
+            </div>
+          </div>
+          <div class="col-lg-4 col-md-4 wow fadeInUp" data-wow-duration=".7s" data-wow-delay=".5s">
+            <div class="single_service">
+              <h3>Online</h3>
+              <p>El cliente no tiene que descargar ninguna App, el QR lo redirige directo a la carta.</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--/ service_content_area  -->
+
+    <!-- about  -->
+    <div class="features_area">
+      <div class="container">
+        <div class="features_main_wrap">
+          <div class="row align-items-center">
+            <div class="col-xl-5 col-lg-5 col-md-6">
+              <div class="features_info2">
+                <h3>La sensación de lo real</h3>
+                <!-- Features that give you real feel -->
+                <p>Incluímos las fotos de tus productos, características y promociones, renovando la experiencia de tus clientes.</p>
+                <!-- renovando la forma de elegir de tus clientes -->
+                <!-- <div class="about_btn">
+                                <a class="boxed-btn4" href="#">Download Now</a>
+                </div>-->
+              </div>
+            </div>
+            <div class="col-xl-5 col-lg-5 offset-xl-1 offset-lg-1 col-md-6">
+              <div class="about_draw wow fadeInUp" data-wow-duration=".7s" data-wow-delay=".5s">
+                <img src="img/ilstrator_img/draw.png" alt />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="features_main_wrap">
+          <div class="row align-items-center">
+            <div class="col-xl-6 col-lg-6 col-md-6">
+              <div class="about_image wow fadeInLeft" data-wow-duration=".4s" data-wow-delay=".3s">
+                <img src="img/ilstrator_img/phone.png" style="max-width: 100%;" alt />
+              </div>
+            </div>
+            <div class="col-xl-6 col-lg-6 col-md-6">
+              <div class="features_info">
+                <h3
+                  class="wow fadeInUp"
+                  data-wow-duration=".5s"
+                  data-wow-delay=".3s"
+                >Fácil de elegir,</h3>
+                <h3 class="wow fadeInUp" data-wow-duration=".5s" data-wow-delay=".3s">Fácil de pagar</h3>
+                <p
+                  class="wow fadeInUp"
+                  data-wow-duration=".6s"
+                  data-wow-delay=".4s"
+                >Con tu App web, podes recibir pedidos del salón y para delivery. Sí, todo en uno!</p>
+                <ul>
+                  <li
+                    class="wow fadeInUp"
+                    data-wow-duration=".7s"
+                    data-wow-delay=".5s"
+                  >El diseño personalizado guía a tus clientes a elegir de una forma cómoda</li>
+                  <li
+                    class="wow fadeInUp"
+                    data-wow-duration=".8s"
+                    data-wow-delay=".6s"
+                  >Ofreces pagos con tarjeta de forma segura</li>
+                  <li
+                    class="wow fadeInUp"
+                    data-wow-duration=".9s"
+                    data-wow-delay=".7s"
+                  >Ahorras las grandes comisiones de otras Apps</li>
+                  <li
+                    class="wow fadeInUp"
+                    data-wow-duration=".10s"
+                    data-wow-delay=".6s"
+                  >Sin descargas, QR y link con el nombre de tu negocio.</li>
+                </ul>
+
+                <!-- <div class="about_btn wow fadeInUp" data-wow-duration=".10s" data-wow-delay=".8s" >
+                                <a class="boxed-btn4" href="#">Download Now</a>
+                </div>-->
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--/ about  -->
+
+    <!-- prising_area  -->
+    <div id="prising_area" class="prising_area">
+      <div class="container">
+        <div class="row">
+          <div class="col-xl-12">
+            <div
+              class="section_title text-center wow fadeInUp"
+              data-wow-duration=".5s"
+              data-wow-delay=".5s"
+            >
+              <h3>Elegí tu plan.</h3>
+              <p>
+                No vas a pagar comisiones por tus ventas,
+                <br />sólo una suscripción.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div class="row no-gutters">
+          <div class="col-xl-6 col-md-6 px-3">
+            <div
+              class="single_prising text-center wow fadeInUp"
+              data-wow-duration=".7s"
+              data-wow-delay=".8s"
+            >
+              <div class="prising_header">
+                <h3>Carta QR</h3>
+                <span>GRATIS</span>
+              </div>
+              <ul>
+                <li>QR con Carta Digital</li>
+                <li>Se muestra fotos, categorías, promociones</li>
+                <li>Mostrá tu marca y tu logo</li>
+                <li>Colores a tu elección</li>
+                <li>Autoadministrable</li>
+                <li>
+                  <strong>Sin descargas!</strong>
+                </li>
+              </ul>
+              <!-- <div class="prising_bottom">
+                            <a href="#" class="get_now prising_btn">Get Now</a>
+              </div>-->
+            </div>
+          </div>
+          <div class="col-xl-6 col-md-6 px-3">
+            <div
+              class="single_prising text-center wow fadeInUp"
+              data-wow-duration=".7s"
+              data-wow-delay=".8s"
+            >
+              <div class="prising_header green_header">
+                <h3>App Web de Pedidos</h3>
+                <span>$1000</span>
+              </div>
+              <ul>
+                <li>App con Carta Digital QR</li>
+                <!-- <li>Usá tu propio dominio</li> -->
+                <li>Se muestran fotos, categorías, promociones</li>
+                <!-- <li>Colores a tu elección </li> -->
+                <li>Mostrá tu marca y tu logo</li>
+                <li>
+                  Disponible para
+                  <strong>pedidos del salón y delivery</strong>
+                </li>
+                <!-- <li>Autoadministrable</li> -->
+                <li>Gestor de pedidos para el cajero</li>
+                <li>
+                  Integrada con plataforma de
+                  <strong>pago</strong> confiable
+                </li>
+                <li>Evitá los porcentajes excesivos en comisiones</li>
+              </ul>
+              <!-- <div class="prising_bottom">
+                            <a href="#contact_area" class="get_now prising_btn">Get Now</a>
+              </div>-->
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--/ prising_area  -->
+
+    <!-- partners_area  -->
+    <div id="partners_area" class="partners_area mt-5">
+      <div class="container">
+        <div class="row">
+          <div class="col 12" style="text-align: center;">
+            <h3>Nuestros Clientes</h3>
+            <div>
+              <img width="120" src="img/gallery/barrilito.jpeg" alt />
+              <img width="120" src="img/gallery/maricel-monroe.jpg" alt />
+              <img width="120" src="img/gallery/plazadelcarmen.png" alt />
+              <img width="120" src="img/gallery/plazadelsol.jpeg" alt />
+              <img width="120" src="img/gallery/tazones.png" alt />
+              <img width="120" src="img/gallery/tunin.jpeg" alt />
+              <img width="120" src="img/gallery/flambo.png" alt />
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- /partners_area  -->
+
+    <!-- contact_area  -->
+    <div id="contact_area" class="contact_area">
+      <div class="container">
+        <div class="row">
+          <div class="col-12">
+            <div
+              class="section_title wow fadeInUp"
+              data-wow-duration=".5s"
+              data-wow-delay=".5s"
+              style="visibility: visible; animation-duration: 0.5s; animation-delay: 0.5s;"
+            >
+              <div class="text-center">
+                <h3>Contactanos!</h3>
+                <p>
+                  Contactate con nosotros para trabajar juntos y
+                  <br />optimizar la tecnología de tu negocio
+                </p>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-12">
+            <div class="d-flex justify-content-center">
+              <div class="media contact-info w-100">
+                <span class="contact-info__icon">
+                  <i class="ti-email"></i>
+                </span>
+                <div class="media-body">
+                  <h3>Email</h3>
+                  <a
+                    href="mailto:contacto@onion.com.ar"
+                    class="genric-btn info-border radius d-block d-md-inline-block"
+                    style="margin-top: 10px;"
+                  >contacto@onion.com.ar</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="col-md-6 col-12">
+            <div class="d-flex justify-content-center">
+              <div class="media contact-info w-100">
+                <span class="contact-info__icon">
+                  <i class="ti-tablet"></i>
+                </span>
+                <div class="media-body">
+                  <h3>Telefono</h3>
+                  <a
+                    href="https://wa.me/5491154876345"
+                    class="genric-btn info-border radius d-block d-md-inline-block"
+                    style="margin-top: 10px;"
+                  >011 54876345</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!--/ contact_area  -->
+
+    <!-- footer start -->
+    <footer class="footer">
+      <div class="footer_top">
+        <div class="container">
+          <div class="row">
+            <div class="col-xl-4 col-md-6 col-lg-4">
+              <div class="footer_widget">
+                <div class="footer_logo">
+                  <a href="#">
+                    <img src="img/logo.png" alt />
+                  </a>
+                </div>
+                <p>De la App a tu mesa</p>
+                <!-- <div class="socail_links">
+                                <ul>
+                                    <li>
+                                        <a href="#">
+                                            <i class="ti-facebook"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="ti-twitter-alt"></i>
+                                        </a>
+                                    </li>
+                                    <li>
+                                        <a href="#">
+                                            <i class="fa fa-instagram"></i>
+                                        </a>
+                                    </li>
+                                </ul>
+                </div>-->
+              </div>
+            </div>
+            <div class="col-xl-4 offset-xl-1 col-md-6 col-lg-2">
+              <div class="footer_widget">
+                <h3 class="footer_title">Links Utiles</h3>
+                <ul>
+                  <li>
+                    <a href="#prising_area">Planes</a>
+                  </li>
+                  <li>
+                    <a href="#service_area_2">Caracteristicas</a>
+                  </li>
+                  <li>
+                    <a href="#contact_area">Contacto</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+            <div class="col-xl-3 col-md-6 col-lg-3">
+              <div class="footer_widget">
+                <h3 class="footer_title">Contacto</h3>
+                <ul>
+                  <li>
+                    <a href="mailto:contacto@onion.com.ar">contacto@onion.com.ar</a>
+                  </li>
+                  <li>
+                    <a href="https://wa.me/5491154876345">+54 911 54876345</a>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="copy-right_text">
+        <div class="container">
+          <div class="footer_border"></div>
+          <div class="row">
+            <div class="col-xl-12">
+            </div>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!--/ footer end  -->
+  </div>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
-import VuetifyLogo from '~/components/VuetifyLogo.vue'
-
 export default {
-  components: {
-    Logo,
-    VuetifyLogo,
-  },
+  layout: 'blank',
 }
 </script>
+
+<style>
+.slicknav_btn {
+  position: fixed !important;
+  top: 25px !important;
+}
+</style>
