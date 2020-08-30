08403556aef107a55b5a6e11195fabd77a9bb0a5
Agustin Amato <agustin.amato96@gmail.com>
2020-08-30 03:45:00 -0300

    [layout] ajustes menores

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,47 +1,17 @@
 <template>
   <v-app>
-    <!-- <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
-      <v-list>
-        <v-list-item v-for="(item, i) in items" :key="i" :to="item.to" router exact>
-          <v-list-item-action>
-            <v-icon>{{ item.icon }}</v-icon>
-          </v-list-item-action>
-          <v-list-item-content>
-            <v-list-item-title v-text="item.title" />
-          </v-list-item-content>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>-->
 
     <v-app-bar fixed app prominent :src="`${data.assets_dirname}background.jpg`">
-      <!-- <v-app-bar-nav-icon @click.stop="drawer = !drawer" /> -->
-
-      <!-- <v-toolbar-title v-text="title" /> -->
-
-      <!-- <v-card>
-        <v-tabs dark background-color="teal darken-3" show-arrows>
-          <v-tabs-slider color="teal lighten-3"></v-tabs-slider>
-
-          <v-tab v-for="i in 30" :key="i" :href="'#tab-' + i">Item {{ i }}</v-tab>
-        </v-tabs>
-      </v-card>-->
 
       <template v-slot:extension>
         <v-card>
           <v-tabs dark show-arrows>
             <v-tabs-slider></v-tabs-slider>
 
-            <!-- <template v-for="rubro in rubros">
-              <nuxt-link :to="'#' + rubro.nombre" :key="rubro.nombre">
-                <v-tab :key="rubro.nombre" :href="'#' + rubro.nombre">{{rubro.nombre}}</v-tab>
+            <v-tab color="#fff" class="white--text" v-for="rubro in rubros" :key="rubro.nombre" :href="'#' + rubro.name">
+              <nuxt-link :to="'#' + rubro.name ">
+                <span class="v-tab__personalized white--text">{{rubro.name}}</span>
               </nuxt-link>
-            </template>-->
-
-            <v-tab color="#fff" class="white--text" v-for="rubro in rubros" :key="rubro.nombre" :href="'#' + rubro.nombre">
-              <nuxt-link :to="'#' + rubro.nombre ">
-                <span class="v-tab__personalized white--text">{{rubro.nombre}}</span>
-              </nuxt-link>
-              <!-- {{rubro.nombre}} -->
             </v-tab>
           </v-tabs>
         </v-card>
