59982f1ea627cfd6a7387c944295a3f0ca213d6c
Agustin Amato <agustin.amato96@gmail.com>
2020-08-30 05:29:22 -0300

    [css] disminuyo blur background.jpg a 2px y ajustes menores sobre app-bar

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,22 +1,39 @@
 <template>
   <v-app>
+    <v-card class="overflow-hidden">
+      <v-app-bar app prominent shrink-on-scroll scroll-threshold="500" :src="`${data.assets_dirname}background.jpg`">
+        <v-spacer></v-spacer>
 
-    <v-app-bar fixed app prominent :src="`${data.assets_dirname}background.jpg`">
+        <!-- // ! Desarrollar barra de busqueda -->
+        <!-- <v-btn icon>
+          <v-icon>mdi-magnify</v-icon>
+        </v-btn>-->
 
-      <template v-slot:extension>
-        <v-card>
-          <v-tabs dark show-arrows>
-            <v-tabs-slider></v-tabs-slider>
+        <template v-slot:img="{ props }">
+          <v-img v-bind="props" gradient="to top, rgba(30,30,30,.2), rgba(99,99,99,.0)"></v-img>
+        </template>
 
-            <v-tab color="#fff" class="white--text" v-for="rubro in rubros" :key="rubro.nombre" :href="'#' + rubro.name">
-              <nuxt-link :to="'#' + rubro.name ">
-                <span class="v-tab__personalized white--text">{{rubro.name}}</span>
-              </nuxt-link>
-            </v-tab>
-          </v-tabs>
-        </v-card>
-      </template>
-    </v-app-bar>
+        <template v-slot:extension>
+          <v-card>
+            <v-tabs dark show-arrows>
+              <v-tabs-slider></v-tabs-slider>
+
+              <v-tab
+                color="#fff"
+                class="white--text"
+                v-for="rubro in rubros"
+                :key="rubro.nombre"
+                :href="'#' + rubro.name"
+              >
+                <nuxt-link :to="'#' + rubro.name ">
+                  <span class="v-tab__personalized white--text">{{rubro.name}}</span>
+                </nuxt-link>
+              </v-tab>
+            </v-tabs>
+          </v-card>
+        </template>
+      </v-app-bar>
+    </v-card>
 
     <v-main>
       <div>
