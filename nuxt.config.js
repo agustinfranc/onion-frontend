d1f044adeb223fcb69d82605ee8405b1080771ba
Agustin Amato <agustin.amato96@gmail.com>
2020-09-17 12:21:49 -0300

    [nuxt] agrego agnes

--- nuxt.config.js
+++ nuxt.config.js
@@ -28,7 +28,7 @@ export default {
   ** See https://nuxtjs.org/api/configuration-generate
   */
   generate: {
-    routes: ['/marlon', '/desembarco', '/newharbor', '/dakota', '/pepper', '/gusto']
+    routes: ['/marlon', '/desembarco', '/newharbor', '/dakota', '/pepper', '/gusto', '/agnes']
   },
   /*
   ** Headers of the page
