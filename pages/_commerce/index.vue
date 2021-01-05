d72f42ae779e9fbda5006a0fcef4b52aef174748
Agustin Amato <agustin.amato96@gmail.com>
2021-01-05 02:19:34 -0300

    agrego skeleton chips

--- pages/_commerce/index.vue
+++ pages/_commerce/index.vue
@@ -1,9 +1,15 @@
 <template>
   <div>
     <template v-if="$fetchState.pending">
-      <!-- <template> -->
-
       <v-container>
+        <v-skeleton-loader v-bind="attrs" type="heading"></v-skeleton-loader>
+
+        <v-skeleton-loader v-bind="attrs" type="chip@4"></v-skeleton-loader>
+
+        <v-divider class="mb-3"></v-divider>
+
+        <v-skeleton-loader v-bind="attrs" type="heading"></v-skeleton-loader>
+
         <v-row>
           <v-col v-for="p in 2" :key="p" cols="6" md="4">
             <v-sheet class="pa-3">
@@ -12,8 +18,14 @@
           </v-col>
         </v-row>
 
-        <v-sheet v-for="p in 10" :key="p" class="pa-3">
+        <v-divider class="mb-3"></v-divider>
+
+        <v-skeleton-loader v-bind="attrs" type="heading"></v-skeleton-loader>
+
+        <v-sheet class="pa-3">
           <v-skeleton-loader
+            v-for="p in 8"
+            :key="p"
             v-bind="attrs"
             type="list-item-avatar-three-line"
           ></v-skeleton-loader>
@@ -288,5 +300,11 @@ export default {
 
 .v-slide-group__wrapper {
   touch-action: pan-y !important;
+}
+
+.v-skeleton-loader__chip {
+  display: inline-block;
+  margin-right: 4px !important;
+  margin-left: 4px !important;
 }
 </style>
